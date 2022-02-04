013db3d0068c7c218acb06b3264a85e8050fd02b
Nick <86270545+NArredondo89@users.noreply.github.com>
2022-02-03 23:50:16 -0800

    added bottom sections for fitness facility

--- src/components/Pages/FitnessFacility.js
+++ src/components/Pages/FitnessFacility.js
@@ -4,10 +4,10 @@ import styled from 'styled-components';
 
 function FitnessFacility(props) {
   return (
-    <>
-      <HeroContainer>
-        <FFImage />
-      </HeroContainer>
+    <FitnessFacilityContainer>
+      {/* <HeroContainer> */}
+      <FFImage />
+      {/* </HeroContainer> */}
       <ContentContainer>
         <SloganContainer>
           <BasketBallTitle>Keep Calm And Ball On</BasketBallTitle>
@@ -15,22 +15,37 @@ function FitnessFacility(props) {
             Professional Caliber Facility. Maintained on a daily basis. We'll
             bring the Equiptment. You bring your A game.
           </BasketBallContent>
-          <BBFunnyLine>New Ankles Not Included</BBFunnyLine>
+          <BBFunnyLine>New Ankles Not Included.</BBFunnyLine>
         </SloganContainer>
         <ImageWrapper>
           <BBImage />
         </ImageWrapper>
       </ContentContainer>
-    </>
+      <GroupExerciseContainer>
+        <GXContentContainer>
+          <GXContent>
+            <GXTitle1>Can't Make Prime Time</GXTitle1>
+            <GXTitle2>Take A Class Anytime</GXTitle2>
+          </GXContent>
+        </GXContentContainer>
+        <GXImageContainer>
+          <GXImage />
+        </GXImageContainer>
+      </GroupExerciseContainer>
+    </FitnessFacilityContainer>
   );
 }
 
 export default FitnessFacility;
 
-const HeroContainer = styled.div`
+const FitnessFacilityContainer = styled.div`
   height: 100vh;
-  z-index: 0;
 `;
+
+// const HeroContainer = styled.div`
+//   height: 100vh;
+//   z-index: 0;
+// `;
 
 const FFImage = styled.div`
   background-image: url(${'https://i.imgur.com/4eFUWrp.jpg'});
@@ -84,7 +99,44 @@ const ImageWrapper = styled.div`
 `;
 
 const BBImage = styled.div`
+  background-image: url(${'https://i.imgur.com/0gcFWLj.png'});
   border: 1px solid white;
+  min-height: 100%;
+  position: relative;
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+const GroupExerciseContainer = styled.div`
+  height: 100vh;
+  background-color: black;
+  color: white;
+`;
+const GXContentContainer = styled.div`
+  padding: 10vh 0 0 0;
+  height: 50vh;
+  width: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: black;
+`;
+const GXContent = styled.div`
+  font-size: 3rem;
+  align-items: center;
+`;
+
+const GXTitle1 = styled.div``;
+const GXTitle2 = styled.div``;
+
+const GXImageContainer = styled.div`
+  height: 50vh;
+  width: 100%;
+`;
+const GXImage = styled.div`
+  background-image: url(${'https://i.imgur.com/GAKfLMw.jpg'});
   min-height: 100%;
   position: relative;
   background-attachment: fixed;
