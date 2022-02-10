a87461ae901c1ad5139968df63639d9dc752155c
Nick <86270545+NArredondo89@users.noreply.github.com>
2022-02-09 23:20:40 -0800

    trying to render image from object

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,53 @@
+export const homeObjOne = {
+  id: 'about',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'The Cafe At Hub925',
+  headline: 'Unlimited Transactions with zero fees',
+  description:
+    'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees',
+  buttonLabel: 'Get Started',
+  imgStart: false,
+  img: require('../../images/cafe.jpg'),
+  alt: 'Car',
+  dark: true,
+  primary: true,
+  darkText: false,
+};
+
+export const homeObjTwo = {
+  id: 'discover',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Unlimited Access',
+  headline: 'Login to your account at any time',
+  description:
+    'We have you covered no matter where you are located. All you need is an internet connection and a phone or computer.',
+  buttonLabel: 'Learn More',
+  imgStart: true,
+  img: require('../../images/Swimming.jpg'),
+  alt: 'Piggybank',
+  dark: true,
+  primary: true,
+  darkText: false,
+};
+
+export const homeObjFour = {
+  id: 'basketball',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Hub925 BasketBall Court',
+  headline: 'Practice Winning Every Day',
+  description:
+    'Professional Caliber Facility. Maintained on a Daily Basis. Well Bring The Equiptment. You Bring Your A Game. New Ankles Not Included',
+  buttonLabel: 'Get Started',
+  imgStart: false,
+  img: require('../../images/Basketball.jpg'),
+  alt: 'Car',
+  dark: true,
+  primary: true,
+  darkText: false,
+};
--- src/components/Pages/FitnessFacility.js
+++ src/components/Pages/FitnessFacility.js
@@ -1,10 +1,10 @@
 import HeroContainer from '../HeroContainer/Index.js';
 
-import ImageContainer from '../ImageContainer/Index';
+import ImageContainer from '../ImageContainer/Index.js';
 import InfoSection from '../InfoSection/Index.js';
 
-import { HeroObjOne } from '../HeroContainer/Data';
-import { homeObjOne, homeObjTwo, homeObjFour } from '../InfoSection/Data.';
+import { homeObjOne, homeObjTwo, homeObjFour } from '../InfoSection/Data.js';
+import { HeroObjOne } from '../HeroContainer/Data.js';
 
 function FitnessFacility() {
   return (
